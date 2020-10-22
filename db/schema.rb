5d664159875a13d84892d362144d643ec8216a5e
RNtaate <royhyde14@gmail.com>
2020-10-22 13:33:47 +0300

    Create Book_group model and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_101517) do
+ActiveRecord::Schema.define(version: 2020_10_22_103055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2020_10_22_101517) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "book_groups", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_groups_on_book_id"
+    t.index ["group_id"], name: "index_book_groups_on_group_id"
   end
 
   create_table "books", force: :cascade do |t|
