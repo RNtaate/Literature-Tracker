501adc34f4bb9bbb0a1a344e134c622c2ff86715
RNtaate <royhyde14@gmail.com>
2020-10-22 22:02:14 +0300

    Update users controller routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :groups
   resources :books
-  resources :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "users#show"
