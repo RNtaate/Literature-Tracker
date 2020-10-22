dca366792d9e7b5533158251a9cb1680c104b114
RNtaate <royhyde14@gmail.com>
2020-10-22 15:56:24 +0300

    Add sign_up_params, account_update_params methods in registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :image)
+  end
+end
