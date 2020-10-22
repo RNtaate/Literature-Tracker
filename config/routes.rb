4270e082703afb575cc1c078b2efead2c8b6e164
RNtaate <royhyde14@gmail.com>
2020-10-22 17:45:30 +0300

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "books#index"
+  root "users#show"
 end
