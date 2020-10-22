b11031d0b262517e2618f38eaf4f7f7b7bf9ba1b
RNtaate <royhyde14@gmail.com>
2020-10-22 16:07:22 +0300

    Add associations with group and book models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups
+  
+  has_many :bought_books, foreign_key: :author_id, class_name: 'Book'
 end
