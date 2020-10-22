1cea6428ec5a38f1938ed37e159534a0ff15c3c6
RNtaate <royhyde14@gmail.com>
2020-10-22 16:59:03 +0300

    Add routing for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :groups
   resources :books
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "books#index"
