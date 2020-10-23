5e96cc5e25e071a3834d63b614b4e2595c411044
RNtaate <royhyde14@gmail.com>
2020-10-23 23:25:06 +0300

    Add index method to users controller and create its routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
  
+  def index
+    redirect_to root_path
+  end
+
   def show
     @user = User.find(current_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :groups
   resources :books
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "users#show"
