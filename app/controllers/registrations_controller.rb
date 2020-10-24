dbf642b54cf7fbe4d1f86a682e99139c80ce833f
RNtaate <royhyde14@gmail.com>
2020-10-24 23:07:12 +0300

    Update account_update_params method in registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :image)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :image, :current_password)
   end
 end
