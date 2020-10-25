e90472ce6d310541dbd55c1c7133af5b41b233e1
RNtaate <royhyde14@gmail.com>
2020-10-25 14:11:45 +0300

    Add community, reader methods to users controller and their routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
  
   def index
-    redirect_to root_path
+  
   end
 
   def show
     @user = User.find(current_user.id)
   end
 
+  def community
+    @users = User.all.with_attached_image.all
+  end
+
+  def reader
+    @user = User.find(params[:id])
+  end
 end
