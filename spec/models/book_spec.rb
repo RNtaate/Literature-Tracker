c39de661705d3ab521350f01fe448179280d021e
RNtaate <royhyde14@gmail.com>
2020-10-25 22:59:33 +0300

    Add uinit tests for user, group, book models

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  let!(:user){User.create!(name: 'user1', email: 'user1@something.com', password: '123456')}
+  context 'Validates the presence of name' do
+    it 'Should be valid' do
+      @book = Book.create(name: 'Thunder', author_id: user.id, book_author: 'Clouds', amount: 45, time: 3)
+      expect(@book.save).to be true
+    end
+
+    it 'Should not be valid' do
+      @book = Book.create(name: '', author_id: user.id, book_author: 'Clouds', amount: 45, time: 3)
+      expect(@book.save).not_to be true
+    end
+  end
+
+  context 'Association between book and group model' do
+    it 'Enables user to assign many groups to a book' do
+      @book = Book.create!(name: 'Thunder', author_id: user.id, book_author: 'Clouds', amount: 45, time: 3)
+
+      @group1 = Group.create!(name: 'Educational', user_id: user.id)
+      @group2 = Group.create!(name: 'Developmental', user_id: user.id)
+
+      @book.groups << @group1
+      @book.groups << @group2
+
+      expect(@book.groups.second.name).to eql('Developmental')
+    end
+  end
+end
---
+++ spec/models/group_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  let!(:user){User.create!(name: 'user1', email: 'user1@something.com', password: '123456')}
+
+  context 'Validates the presence of name' do
+    it 'Should be valid' do
+      @group = Group.create(name: 'Educational', user_id: user.id)
+      expect(@group.save).to be true
+    end
+
+    it 'Should not be valid' do
+      @group = Group.create(name: '', user_id: user.id)
+      expect(@group.save).not_to be true
+    end
+  end
+
+  context 'Association between group and book model' do
+    it 'Enables user to assign many books to a group' do
+      @group = Group.create!(name: 'Educational', user_id: user.id)
+
+      @book1 = Book.create!(name: 'Thunder', author_id: user.id, book_author: 'Clouds', amount: 45, time: 3)
+      @book2 = Book.create(name: 'Lightning', author_id: user.id, book_author: 'Clouds', amount: 45, time: 3)
+
+      @group.books << @book1
+      @group.books << @book2
+
+      expect(@group.books.second.name).to eql('Lightning')
+    end
+  end
+end
