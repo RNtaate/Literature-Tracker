02edccab74641d24475dfbabb045bb0ec572ecf0
RNtaate <royhyde14@gmail.com>
2020-10-28 02:39:14 +0300

    Create ungrouped_books, ungrouped_sum class methods in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,26 @@ class Book < ApplicationRecord
   has_many :groups, through: :book_groups
 
   belongs_to :author, class_name: 'User'
+
+  def self.ungrouped_books(current_user)
+    @all_books = Book.where_id_is(current_user.id).includes(:groups)
+    @books = []
+
+    @all_books.each do |book|
+      @books << book if book.groups.empty?
+    end
+
+    @books
+  end
+
+  def self.ungrouped_sum(current_user, column)
+    sum = 0
+    case column
+    when 'amount'
+      Book.ungrouped_books(current_user).each { |a| sum += a.amount }
+    when 'time'
+      Book.ungrouped_books(current_user).each { |a| sum += a.time }
+    end
+    sum
+  end
 end
