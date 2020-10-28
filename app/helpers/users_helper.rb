45a4dc65c61e4b13743ba70f757634904c6c1b64
RNtaate <royhyde14@gmail.com>
2020-10-29 02:22:12 +0300

    Remove all logic from users community, reader view templates

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,37 @@
+module UsersHelper
+
+  def display_users_list(users)  
+    users_list = ''
+    users.each do |user|
+      users_list << "
+      <a href='#{reader_path(user)}'>
+        <div class='list-item'> 
+  
+          #{display_user_image(user, 'image-div', '/assets/group2.png')}
+  
+          <h4 class='list-item-details book-name-date'>#{user.name}</h4>
+        </div>
+      </a>"
+    end
+
+    users_list.html_safe
+  end
+
+  def display_reader(user)
+    single_user = ''
+
+    @user.books.each do |book|
+      single_user << "
+      <div class='list-item'>
+        <div class='list-item-details group-name-date'>
+            <p>'#{book.name }'</p>
+            <small>Author: <span>#{ book.book_author }</span></small>
+          </div>
+        </div>
+      </div>" 
+    end
+
+    single_user.html_safe
+  end
+
+end
